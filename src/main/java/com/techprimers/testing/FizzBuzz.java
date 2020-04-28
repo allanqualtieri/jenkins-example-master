e2b91b1396d09b5cb114b426d967e0df64256d1a
Allan Qualtieri <allanqualtieri@gmail.com>
2020-04-28 16:48:33 -0300

    top

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -2,18 +2,13 @@ package com.techprimers.testing;
 
 public class FizzBuzz {
 
-  //  public String play(int number) {
+    public String play(int number) {
 
-    //    if (number == 0) throw new IllegalArgumentException("Number must not be 0");
-    //    if (number % 3 == 0) return "Fizz";
-     //   if (number % 5 == 0) return "Buzz";
+        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
 
 
-//        return String.valueOf(number);
-  //  }
-  
-  public String play() {
-
-        return "Fizz";
-  //  }
+        return String.valueOf(number);
+    }
 }
